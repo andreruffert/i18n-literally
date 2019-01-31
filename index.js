d630e1739f6b8510926d9c05cfe456f3de025a8a
André Ruffert <andre@andreruffert.com>
2019-01-31 01:17:52 +0100

    feat: introduce i18n tag fn

---
+++ index.js
@@ -0,0 +1,14 @@
+function i18n(strings, ...values) {
+  const key = strings.join('\x01');
+  const translation = (i18n.db[key] && i18n.db[key][i18n.locale]) || strings;
+  return translation.map((string, idx) => [string, values[idx]])
+    .reduce((acc, val) => acc.concat(val), [])
+    .join('');
+
+};
+
+// Defaults
+i18n.locale = 'en';
+i18n.db = {};
+
+export default i18n;
