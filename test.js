0d0870aa21abaf854aeac3f8de5d9c8bcbe3a36a
André Ruffert <andre@andreruffert.com>
2019-02-04 12:12:34 +0100

    test: add mergeDB tests

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 import test from 'ava';
 import i18n from './index';
+import { mergeDB } from './cli/helpers';
 import db from './i18n.db.json';
 
 // Define db
@@ -12,10 +13,6 @@ const data = [
   '✌️'
 ];
 
-// TODO:
-// cli test with empty db file
-// cli test db merge
-
 test('locale change', t => {
   i18n.locale = 'es';
 	t.is(i18n`Hello ${data[0]}!`, `Hola ${data[0]}!`);
@@ -27,4 +24,16 @@ test('locale change', t => {
 test('nested tags', t => {
   const template = `Hello test ${data[0]}... ${i18n`${data[1]} - ${data[2]}... Such wow!`}`;
 	t.is(i18n`${template}`, template);
+});
+
+test('CLI mergeDB', t => {
+  const data = {
+    de: 'Hallo'.split(' '),
+    es: 'Hola'.split(' ')
+  };
+  const existingDB = {'token': { 'de': data.de }};
+  const updatedDB = {'token': { 'es': data.es }};
+  const mergedDB = {'token': { de: data.de, es: data.es }};
+	t.deepEqual(mergeDB(existingDB, updatedDB), mergedDB);
+	t.deepEqual(mergeDB({}, updatedDB), updatedDB);
 });
